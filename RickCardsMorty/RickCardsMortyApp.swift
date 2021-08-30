0ed3f5ed8c24c4a5a7ffb92cbe54877cf9c6b993
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-08-30 14:52:26 -0500

    Initial Commit

---
+++ RickCardsMorty/RickCardsMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickCardsMortyApp.swift
+//  RickCardsMorty
+//
+//  Created by HanzoMac on 30/08/21.
+//
+
+import SwiftUI
+
+@main
+struct RickCardsMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
